e0d32022a5f71af181fd718f05c57ba4f4447d7f
Yoga Yudha Tama <yogayudhatama48@gmail.com>
2024-12-20 15:04:33 +0700

    update

--- validate.py
+++ validate.py
@@ -1,8 +1,10 @@
 # validate.py
-# Fungsi melakukan validasi terhadap S-Box 
+# Fungsi melakukan validasi terhadap S-Box
 def validate_sbox(s_box):
     if len(s_box) != 256:
-        return False, 
+        return False, "S-Box harus memiliki 256 elemen."
     if len(set(s_box)) != 256:
-        return False, 
-    return True, 
+        return False, "Semua elemen dalam S-Box harus unik."
+    if any(val < 0 or val > 255 for val in s_box):
+        return False, "Nilai dalam S-Box harus berada di antara 0 dan 255."
+    return True, "S-Box valid."
